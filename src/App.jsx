0fc6434571dc20f5b1b3dfc3439d03a0281e3772
Rachel Hamby <tnthreat33@gmail.com>
2023-12-16 11:01:38 -0500

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,82 @@
+import Feedback from "./Feedback";
+
+
+const Header = (props) => {
+  return (
+    <h1>{props.course.name}</h1>
+  );
+};
+
+const Part = (props) => {
+  console.log(props.parts.parts.parts[0]);
+  return (
+    <div>
+      <p> {props.parts.parts.parts[0].name}</p>
+      <p> {props.parts.parts.parts[1].name}</p>
+      <p> {props.parts.parts.parts[2].name}</p>
+    </div>
+
+  );
+};
+
+
+const Content = (props) => {
+ 
+  return (
+    <Part parts = {props}/>
+  );
+};
+
+const Total = (props) => {
+  console.log(props);
+  let totalExercises = 0
+  
+  props.parts.parts.forEach((part)=>{totalExercises += part.exercises})
+
+  return(
+  <p> Number of Exercises {totalExercises} </p>
+)}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+  // const course = 'Half Stack application development'
+  // const part1 = {
+  //   name: 'Fundamentals of React',
+  //   exercises: 10
+  // }
+  // const part2 = {
+  //   name: 'Using props to pass data',
+  //   exercises: 7
+  // }
+  // const part3 = {
+  //   name: 'State of a component',
+  //   exercises: 14
+  // }
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts = {course}/>
+      <Total parts = {course}/>
+      <Feedback/>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Feedback.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react"
+
+const Statistics = ({good, neutral, bad}) =>{
+    let all = good + bad + neutral
+    let average = (good + (bad * -1)) / all
+    let positive = good / all
+
+    return(
+        <div>
+        <p> average {isNaN(average) ? 0 : average} </p>
+        <p> positive {isNaN(positive) ? 0 : positive} </p>
+        <p> all {all}</p>
+        </div>
+    )
+}
+
+
+
+
+const Feedback = ()=>{
+    
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+    
+
+   function handleGoodClick (){
+        setGood(good + 1)
+    }
+
+    function handleNeutralClick (){
+        setNeutral(neutral + 1)
+    }
+   
+    function handleBadClick (){
+        setBad(bad + 1)
+    }
+  
+    return (
+      <div>
+        <h2>give feedback</h2>
+        <button onClick ={handleGoodClick}> good </button>
+        <button onClick ={handleNeutralClick}> neutral</button>
+        <button onClick ={handleBadClick}>bad</button>
+        <h2> statistics</h2>
+        
+        {good === 0 && bad === 0 && neutral === 0 ? (
+            "No feedback given"
+        ) : (
+            <div>
+            <p>good {good}</p>
+            <p>neutral {neutral}</p>
+            <p>bad {bad}</p>
+            <Statistics good={good} bad={bad} neutral={neutral} />
+        </div>
+        )}
+
+
+      </div>
+    )
+}
+
+export default Feedback;

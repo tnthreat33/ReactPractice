492d1a28adda86ca272b3ddd97480e323227e572
Rachel Hamby <tnthreat33@gmail.com>
2023-12-18 20:52:46 -0500

    2.3

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,55 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
-  
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas', number: '123456789' }]);
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
 
-  const addName =(e) => {
+  const addName = (e) => {
     e.preventDefault();
-    const personExist = persons.some((person) => person.name === newName)
-    if(personExist){
-       alert(`${newName} is already added to phonebook`)
-     } else{
-       setPersons([...persons, {name: newName}])
-     }
-  }
+    const personExists = persons.some((person) => person.name === newName);
 
-  const handleNameChange= (e) =>{
-    
-    setNewName(e.target.value)
-  }
+    if (personExists) {
+      alert(`${newName} is already added to phonebook`);
+    } else {
+      setPersons([...persons, { name: newName, number: newNumber }]);
+      setNewName('');
+      setNewNumber('');
+    }
+  };
+
+  const handleNameChange = (e) => {
+    setNewName(e.target.value);
+  };
+
+  const handleNumberChange = (e) => {
+    setNewNumber(e.target.value);
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit = {addName}>
+      <form onSubmit={addName}>
         <div>
-          name: <input value = {newName} onChange ={handleNameChange}/>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((name)=>(
-        <ul key = {name.name}>
-          <li> {name.name}</li>
+      {persons.map((person) => (
+        <ul key={person.name}>
+          <li>
+            {person.name} - {person.number}
+          </li>
         </ul>
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;

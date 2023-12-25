044193c1a84f202230e95a3ee914799df8d63d9a
Rachel Hamby <tnthreat33@gmail.com>
2023-12-25 14:31:10 -0500

    2.9

--- src/Feedback.jsx
+++ src/Feedback.jsx
@@ -18,9 +18,7 @@ const FeedbackButtons = ({ handleGoodClick, handleNeutralClick, handleBadClick }
     return (
       <div>
         <button onClick={handleGoodClick}>good</button>
-        
         <button onClick={handleNeutralClick}>neutral</button>
-
         <button onClick={handleBadClick}>bad</button>
         
       </div>
